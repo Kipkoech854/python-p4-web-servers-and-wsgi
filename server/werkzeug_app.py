1642cff2c25b59a93a3701a16e30267c24dc7cd4
kipkoech <gideonkipkoech854@gmail.com>
2025-06-10 19:23:41 +0300

    this is my first commit

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request,Response
+
+@Request.application
+def application(request):
+    print(f'This is a web server running at {request.remote_addr}')
+    return Response('A wsgi generated this response!')
+
+if __name__ == '__main__':
+    from werzeug.serving import run simple
+    run_simple(
+        hostname = 'localhost',
+        port = 5555,
+        application = application
+    )    
